cdbbc0fad068238f911867a1d05256bfdd0d95ea
Hsu Yee Mon <hsuyeemon.1@gmail.com>
2018-02-03 23:39:04 +0630

    Create SearchTedTalkActivity

--- PADC-3-F-HYM-TedTalk/app/src/main/java/xyz/hsuyeemon/tedtalk/activities/SearchTedTalkActivity.java
+++ PADC-3-F-HYM-TedTalk/app/src/main/java/xyz/hsuyeemon/tedtalk/activities/SearchTedTalkActivity.java
@@ -3,7 +3,10 @@ package xyz.hsuyeemon.tedtalk.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import xyz.hsuyeemon.tedtalk.R;
 
@@ -13,12 +16,22 @@ import xyz.hsuyeemon.tedtalk.R;
 
 public class SearchTedTalkActivity extends AppCompatActivity {
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_ted_talk);
 
         ButterKnife.bind(this,this);
-        
+        setSupportActionBar(toolbar);
+
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setIcon(R.drawable.ic_arrow_back_24dp);
+
+        }
+
+
     }
 }
